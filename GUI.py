7f348e80449c020cbd81d09dd74c9ff744f09685
Delbaz Samadian <126827596+DelbazSamadian@users.noreply.github.com>
2025-06-10 10:21:11 +0100

    GUI.py

--- GUI.py
+++ GUI.py
@@ -68,7 +68,7 @@ st.set_page_config(page_title="MIDR Prediction App", layout="wide")
 # Teesside University Branding
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("path/to/tees_logo.png", width=150)  # Replace with correct path
+    st.image("OIP.jpeg", width=150)
 with col2:
     st.markdown("""
     # MIDR Prediction App  
